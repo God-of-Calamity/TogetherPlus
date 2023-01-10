0b34aa95b29f43c0ba9d422567d5d869c89a60af
ADITYA VIKRAM SINGH <82879413+God-of-Calamity@users.noreply.github.com>
2023-01-10 19:36:21 +0530

    Add files via upload

---
+++ js/createGroups.js
@@ -0,0 +1,37 @@
+// Elements
+const $createGroups = document.querySelector('#createGroups-form');
+
+const createGroup = async function () {
+    const data = {
+        username: $createGroups.elements["username"].value,
+        email: $createGroups.elements["email"].value,
+        password: $createGroups.elements["password"].value,
+        groupName: $createGroups.elements["groupName"].value,
+        groupCode: $createGroups.elements["groupCode"].value
+    }
+
+    const response = await fetch(`http://localhost:3000/createGroups`, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+    });
+
+    return response.json();
+}
+
+$createGroups.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    createGroup().then(function (data) {
+        console.log(`Data is.....`);
+        console.log(data);
+        if (data.code) {
+            alert('Group already exists...');
+            location.href = `http://localhost:3000/createGroups.html`;
+        }
+    }).catch(function (error) {
+        console.log(error);
+    })
+});
